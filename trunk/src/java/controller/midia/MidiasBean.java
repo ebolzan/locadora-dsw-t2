f71a319e6e9b5b2cb82ba06a88f541b88d45de09
ebolzan@inf.ufsm.br <ebolzan@inf.ufsm.br@bc1cb96c-b120-be46-8163-c206d0ab712e>
2014-01-10 20:06:59 +0000

    beans alterados

--- trunk/src/java/controller/midia/MidiasBean.java
+++ trunk/src/java/controller/midia/MidiasBean.java
@@ -6,10 +6,75 @@
 
 package controller.midia;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import model.filme.Filmes;
+import model.midia.Midias;
+import model.midia.MidiasDAO;
+
 /**
  *
  * @author evandro
  */
-public class MidiasBean {
+@Named
+@SessionScoped
+public class MidiasBean implements Serializable{
     
+    private MidiasDAO midiasDao;    
+    private char inutilizada;
+    private Filmes filmes;
+
+    public MidiasBean() {
+        this.midiasDao = new MidiasDAO();
+    }
+
+    public MidiasDAO getMidiasDao() {
+        return midiasDao;
+    }
+
+    public void setMidiasDao(MidiasDAO midiasDao) {
+        this.midiasDao = midiasDao;
+    }
+
+    public char getInutilizada() {
+        return inutilizada;
+    }
+
+    public void setInutilizada(char inutilizada) {
+        this.inutilizada = inutilizada;
+    }
+
+    public Filmes getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(Filmes filmes) {
+        this.filmes = filmes;
+    }
+            
+    public List<Midias> getMidiasList()
+    {
+        return this.midiasDao.lista();
+    }
+    
+    public String addAction()
+    {
+        Midias m1 = new Midias(Integer.SIZE);
+        midiasDao.salvar(m1);        
+        return null;
+    }
+    
+    public String deleteAction(Midias m1)
+    {
+        midiasDao.excluir(m1);
+        return null;
+    }        
+    
+    public String updateAction(Midias m1)
+    {
+        midiasDao.atualizar(m1);
+        return null;
+    }       
 }
